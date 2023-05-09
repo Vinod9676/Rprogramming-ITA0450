9f7941c431e4d188a0912867ae525c35cbd52c8c
Vinod9676 <113161879+Vinod9676@users.noreply.github.com>
2023-05-09 10:06:33 +0530

    Add files via upload

---
+++ 1 st program.r
@@ -0,0 +1,7 @@
+name <-readline("enter youur name:")
+age <- readline("enter your age:")
+
+cat("your name is ", name ,"and your age is ", age,"\n")
+
+
+cat("r version is ", R.version$version.string ,"is installed \n")
---
+++ 3rd prog.r
@@ -0,0 +1,11 @@
+seq <- 20:50
+
+mean_1 <- mean(20:60)
+
+total <- sum(51:90)
+
+cat("sequance of numbers from 20:50 :",seq,"\n")
+
+cat("mean of 20:60 is:",mean_1,"\n")
+
+cat("sum of 51:90 is :",total)
